a5bd34d9610db22fdbd19479a0771fc47aada45a
フィルム <waranthorn_c@outlook.com>
2022-01-04 12:43:32 +0700

    Initial commit

---
+++ bot.py
@@ -0,0 +1,38 @@
+import discord
+from discord import Embed, File
+from datetime import datetime
+
+client = discord.Client()
+
+@client.event
+async def on_ready( ):
+    print('bot is ready to use')
+    # c = get_channel('927601384629628960')
+    # await c.send('bot is ready to use')
+
+@client.event
+async def on_message(message):
+    # if message.author == client.user:
+    #     return 'hi'
+    if message.content == 'Woody' or message.content == 'woody':
+        embed = Embed(title='Woody', description='Cowboy', color=0xFF0000, timestamp=datetime.utcnow())
+        fields = [
+                    ('Name', 'Value', True),
+                    ('information', 'vc', True),
+                    ('X', 'Y', True)
+                ]
+        for name, value, inline in fields:
+            embed.add_field(name=name, value=value, inline=True)
+        embed.set_author(name='BOT')
+        embed.set_image(file=File('img/woody.jpeg'))
+        await message.channel.send(embed=embed)
+
+
+
+
+    # if message.content == 'Hi bot':
+    #     await message.channel.send('Greetings sir!')
+
+    # await message.channel.send('Sawasdee')
+
+client.run('OTI3NjAxMzg0NjI5NjI4OTYw.YdMmHA.1PZcvKmzXOntEuOIYwJjZXgkiTw')
